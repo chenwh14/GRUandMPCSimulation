fefbf2f634d95303d78d5a5fdbc42bdc51383e09
chenwh14 <chenwh14.thu@gmail.com>
2020-03-22 11:29:38 +0800

    ignore slprj folder

--- CompileGRUandMPC.m
+++ CompileGRUandMPC.m
@@ -1,5 +1,12 @@
+%%
+%±‡“ÎGRU
 path1='-I C:\Users\cwh\.conan\data\functionalplus\v0.2.6-p0\dobiasd\stable\package\5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9\include';
 path2='-I C:\Users\cwh\.conan\data\frugally-deep\v0.12.1-p0\dobiasd\stable\package\5d27be51bcd7867b53a0a592d51f2dece9cd0b40\include';
 path3='-I C:\Users\cwh\.conan\data\eigen\3.3.4\conan\stable\package\5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9\include\eigen3';
 path4='-I C:\Users\cwh\.conan\data\jsonformoderncpp\3.1.0\vthiery\stable\package\5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9\include';
 mex(path1,path2,path3,path4,'GRUwrapper.cpp')
+%%
+%±‡“ÎMPC
+lpath='-L C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2020.0.166\windows\mkl\lib\intel64_win';
+lib='-lmkl_rt';
+mex(lpath,lib,'MPCwrapper.cpp','MPController.cpp','KalmanFilter.cpp')
